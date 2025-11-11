f374b9d42b844a8729ea9b27db83ce1eb8949ad8
Nutifafa-kofi <ansahishmael399@gmail.com>
2025-11-11 17:27:07 +0000

    warehouse inventory

--- index.js
+++ index.js
@@ -1,31 +1,51 @@
-//Total cost of the order
-function calculateTotalCost(pricePerCup, numberOfCups) {
-  let totalCost = pricePerCup * numberOfCups;
-  return totalCost;
+// //Total cost of the order
+// function calculateTotalCost(pricePerCup, numberOfCups) {
+//   let totalCost = pricePerCup * numberOfCups;
+//   return totalCost;
+// }
+
+// console.log(calculateTotalCost(5, 16));
+
+// //Discount applied
+// function applyDiscount(pricePerCup, numberOfCups, discountPercent) {
+//   let totalCost = calculateTotalCost(pricePerCup, numberOfCups);
+
+//   if (numberOfCups > 10) {
+//     let discountGiven = totalCost * (discountPercent / 100);
+//     let finalPrice = totalCost - discountGiven;
+//     return finalPrice;
+//   } else {
+//     return totalCost;
+//   }
+// }
+
+// console.log(applyDiscount(5, 30, 50));
+
+// //USD to GHS conversion
+// function usdToGHS(pricePerCup,numberOfCups, exchangeRate) {
+//   let totalCost = calculateTotalCost(pricePerCup, numberOfCups);
+//   let GHS = totalCost * exchangeRate;
+//   return GHS;
+// }
+
+// console.log(usdToGHS(5, 16, 10.9));
+
+let shoe = { brand: "Adidas", color: "Red", price: 300, inStock: 10 };
+function newStock(shoe, addedStock) {
+  shoe.inStock += addedStock;
+  return shoe.inStock;
 }
 
-console.log(calculateTotalCost(5, 16));
+console.log(newStock(shoe, 5));
 
-//Discount applied
-function applyDiscount(pricePerCup, numberOfCups, discountPercent) {
-  let totalCost = calculateTotalCost(pricePerCup, numberOfCups);
-
-  if (numberOfCups > 10) {
-    let discountGiven = totalCost * (discountPercent / 100);
-    let finalPrice = totalCost - discountGiven;
-    return finalPrice;
+function stockSold(shoe, quantitySold) {
+  if (shoe.inStock >= quantitySold) {
+    shoe.inStock -= quantitySold;
+   return `${quantitySold} items are sold out`;
   } else {
-    return totalCost;
+    return shoe.inStock;
   }
 }
 
-console.log(applyDiscount(5, 30, 50));
+console.log(stockSold(shoe, 5));
 
-//USD to GHS conversion
-function usdToGHS(pricePerCup,numberOfCups, exchangeRate) {
-  let totalCost = calculateTotalCost(pricePerCup, numberOfCups);
-  let GHS = totalCost * exchangeRate;
-  return GHS;
-}
-
-console.log(usdToGHS(5, 16, 10.9));
